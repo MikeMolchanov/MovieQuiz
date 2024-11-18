c7dcd48fbc80f8b72b125a7b0a03dbf88b928e8b
Mike <misha995@yandex.ru>
2024-11-18 22:08:27 +0300

    вынес convert()

--- MovieQuiz/Presentation/MovieQuizViewController.swift
+++ MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -2,9 +2,10 @@ import UIKit
 
 final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  {
     
+    private let presenter = MovieQuizPresenter()
     private var correctAnswers = 0
-    private var currentQuestionIndex = 0
-    private let questionsAmount: Int = 10
+    
+    
     private var questionFactory: QuestionFactoryProtocol?
     private var currentQuestion: QuizQuestion?
     
@@ -37,7 +38,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
                                buttonText: "Попробовать еще раз") { [weak self] _ in 
                 guard let self = self else { return }
                 
-                self.currentQuestionIndex = 0
+                self.presenter.resetQuestionIndex()
                 self.correctAnswers = 0
                 
                 self.questionFactory?.requestNextQuestion()
@@ -46,12 +47,6 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
         alertPresenter.show(model)
     }
     
-    private func convert(model: QuizQuestion) -> QuizStepViewModel {
-        return QuizStepViewModel(
-            image: UIImage(data: model.image) ?? UIImage(),
-            question: model.text,
-            questionNumber: "\(currentQuestionIndex + 1)/\(questionsAmount)")
-    }
     // приватный метод вывода на экран вопроса, который принимает на вход вью модель вопроса и ничего не возвращает
     private func show(quiz step: QuizStepViewModel) {
         imageView.image = step.image
@@ -87,9 +82,9 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
     // приватный метод, который содержит логику перехода в один из сценариев
     // метод ничего не принимает и ничего не возвращает
     private func showNextQuestionOrResults() {
-        if currentQuestionIndex == questionsAmount - 1 {
+        if presenter.isLastQuestion() {
             let currentDate = Date()
-            let gameResult = GameResult(correct: correctAnswers, total: 10, date: currentDate.dateTimeString)
+            let gameResult = GameResult(correct: correctAnswers, total: presenter.questionsAmount, date: currentDate.dateTimeString)
             statisticService.store(my: gameResult)
             let message = "Ваш результат: \(correctAnswers)/10 \n" +
             "Количество сыгранных квизов: \(statisticService.gamesCount)\n" +
@@ -101,7 +96,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
             buttonText: "Сыграть ещё раз",
             completion: {[weak self] _ in
                 guard let self = self else { return }
-                self.currentQuestionIndex = 0
+                presenter.resetQuestionIndex() 
                 self.correctAnswers = 0
                 
                 guard let questionFactory = self.questionFactory else {
@@ -116,7 +111,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
             
         }
         else {
-            currentQuestionIndex += 1
+            presenter.switchToNextQuestion()
             // идём в состояние "Вопрос показан"
             guard let questionFactory = questionFactory else {
                 return
@@ -136,7 +131,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
         
         let action = UIAlertAction(title: result.buttonText, style: .default) { [weak self] _ in 
             guard let self = self else { return } 
-            self.currentQuestionIndex = 0
+            presenter.resetQuestionIndex()
             self.correctAnswers = 0
             guard let questionFactory = questionFactory else {
                 return
@@ -200,7 +195,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate  
             return
         }
         currentQuestion = question
-        let viewModel = convert(model: question)
+        let viewModel = presenter.convert(model: question)
         
         DispatchQueue.main.async { [weak self] in
             self?.show(quiz: viewModel)
